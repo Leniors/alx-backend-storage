48fa03552a1ca021c9fcc9a81d70437b1332c1ff
Leniors <mokualeeroy@gmail.com>
2024-05-17 04:19:27 +0300

    update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" update documents """
+
+def update_topics(mongo_collection, name, topics):
+    """ update ocuments """
+    mongo_collection.update_many({"name": name}, "$set": {"topics": topics})

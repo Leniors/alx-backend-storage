26dfb6886c89588016347f21f1c3ee63e73a4a76
Leniors <mokualeeroy@gmail.com>
2024-05-17 04:28:13 +0300

    find

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" filter by find """
+
+def schools_by_topic(mongo_collection, topic):
+    """ find schools """
+    my_list = []
+    my_list.extend(mongo_collection.find({"topics": topic}))
